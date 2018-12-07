73c84068f0005e577e44b297efd9780214023324
Wendeline <wendeline.swart.lnd@gmail.com>
2018-12-07 01:54:56 +0100

    tuvasmehair

---
+++ starter-template/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+        <?php include'php/head.php'; ?>
+
+
+<body>
+
+    <?php include 'php/nav.php'; ?>
+
+    <div id="index-banner" class="parallax-container">
+        <div class="section no-pad-bot">
+            <div class="container homeFond z-depth-4">
+                    <br><br>
+                    <h1 class="header center teal-text text-lighten-2 typoLogo">Arts Pluriel</h1>
+                    
+                    <div class="row center">
+                        <h5 class="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+                    </div>
+                
+                <div class="row center">
+                    <a href="#start" id="download-button" class="btn-large waves-effect waves-light teal lighten-1 js-scrollTo">Get Started</a>
+                </div>
+                <br><br>
+            </div>
+        </div>
+
+
+
+        <div id="parallax" class="parallax">
+            <img id="homeImg" class="fadeIn" src="images/bg/bg1.jpg" alt="Unsplashed background img 1">
+        </div>
+    </div>
+
+
+    <div class="container" id="start">
+        <div class="section">
+
+                      
+        </div>
+    </div>
+
+
+
+    <?php
+        include 'php/footer.php';
+        include 'php/script.php';
+    ?>
+
+    <script type="text/javascript">
+    </script>            
+</body>
+</html>
